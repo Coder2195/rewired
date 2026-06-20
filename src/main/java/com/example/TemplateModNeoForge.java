38b16e378220569a75dfdfc2e59cde039661b286
KikuGie <git.kikugie@protonmail.com>
2026-06-21 01:28:50 +0200

    Initial commit

---
+++ src/main/java/com/example/TemplateModNeoForge.java
@@ -0,0 +1,17 @@
+package com.example;
+
+//? if neoforge {
+/*import net.neoforged.fml.common.Mod;
+
+import static com.example.TemplateModCommon.*;
+
+@Mod("template")
+public class TemplateModNeoForge {
+    public TemplateModNeoForge() {
+        LOGGER.info("Hello Fabric world!");
+
+        //? if !release
+        LOGGER.warn("I'm still a template!");
+    }
+}
+*///?}
